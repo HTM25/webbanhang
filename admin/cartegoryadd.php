84596bf2ae30e269faf0e8c7a8e00d54232e55d9
Vuong30102001 <100443011+Vuong30102001@users.noreply.github.com>
2022-05-29 18:32:08 +0700

    vương commit

--- admin/cartegoryadd.php
+++ admin/cartegoryadd.php
@@ -1,7 +1,7 @@
 <?php
 include "header.php";
 include "leftside.php";
-include "class/cartegory_class.php";
+//include "class/cartegory_class.php";
 // define('__ROOT__', dirname(dirname(__FILE__))); 
 // require_once(__ROOT__.'../admin/class/cartegory_class.php');
 ?>
